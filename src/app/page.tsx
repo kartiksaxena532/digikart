400f49bb0f4d3ed807582fa9860bcf17065f348b
Kartik Saxena <karrtiksaxena@gmail.com>
2023-12-26 23:03:14 +0530

    formpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 import Image from 'next/image'
-
+import { Button } from '@/components/ui/button'
+import CardWithForm  from './CardWithForm'
+import { Content } from '@radix-ui/react-select'
 export default function Home() {
+
   return (
-    <p> Helllo kartik </p>
+    <>
+    <div className='flex items-center justify-center '>
+    <h2 className='text-5xl  m-7 font-sans hover:font-mono subpixel-antialiased '>Kartik saxena Page</h2>
+    </div>
+   <div className='flex items-center justify-center'>
+     
+      <CardWithForm/>
+    </div>
+    </>
+    
   )
 }
